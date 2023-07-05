394232156fb88fdee9521db9cfd3d78d1f9ee9a8
Nanda Arya Putra <nandaarya404@gmail.com>
2023-07-05 23:25:51 +0700

    initial commit

---
+++ Day 2/Tugas-Day-2-App/app/src/main/java/com/example/tugas_day_2_app/showData.kt
@@ -0,0 +1,11 @@
+package com.example.tugas_day_2_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class showData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_data)
+    }
+}
