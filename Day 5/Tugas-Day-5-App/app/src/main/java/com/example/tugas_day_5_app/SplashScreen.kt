b2e2a8bf7c751c26bb8fa96ac6cca738eef154c6
Nanda Arya Putra <nandaarya404@gmail.com>
2023-07-09 22:50:26 +0700

    add splashscreen

---
+++ Day 5/Tugas-Day-5-App/app/src/main/java/com/example/tugas_day_5_app/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.example.tugas_day_5_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tugas_day_5_app.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashScreenBinding
+    private val splashTimeOut: Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.root.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
+    }
+}
