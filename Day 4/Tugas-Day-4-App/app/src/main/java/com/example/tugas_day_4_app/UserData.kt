1a076a7e2f05cd3f0b5e4734efd028b78ee5a0dd
Nanda Arya Putra <nandaarya404@gmail.com>
2023-07-09 14:38:27 +0700

    add user data class

---
+++ Day 4/Tugas-Day-4-App/app/src/main/java/com/example/tugas_day_4_app/UserData.kt
@@ -0,0 +1,155 @@
+package com.example.tugas_day_4_app
+
+data class User(
+    val name: String,
+    val email: String,
+    val jurusan: String,
+    val semester: Int,
+    val image: String
+)
+
+val userListA = listOf(
+    User(
+        "Budi Santoso",
+        "BudiSantoso@gmail.com",
+        "Teknik Informatika",
+        1,
+        "https://st.depositphotos.com/2931363/5142/i/450/depositphotos_51425493-stock-photo-student-holding-textbooks.jpg"
+    ),
+    User(
+        "Rina Kartika",
+        "RinaKartika@gmail.com",
+        "Akuntansi",
+        2,
+        "https://st4.depositphotos.com/2760050/38295/i/450/depositphotos_382951958-stock-photo-library-makes-you-visionary-library.jpg"
+    ),
+    User(
+        "Ahmad Hidayat",
+        "AhmadHidayat@gmail.com",
+        "Psikologi",
+        3,
+        "https://st2.depositphotos.com/4431055/11853/i/450/depositphotos_118536368-stock-photo-student-man-with-laptop.jpg"
+    ),
+    User(
+        "Siti Aisyah",
+        "SitiAisyah@gmail.com",
+        "Ilmu Komunikasi",
+        4,
+        "https://st2.depositphotos.com/1594308/12210/i/450/depositphotos_122104332-stock-photo-female-college-student.jpg"
+    ),
+    User(
+        "Joko Prasetyo",
+        "JokoPrasetyo@gmail.com",
+        "Manajemen",
+        5,
+        "https://st.depositphotos.com/1715570/5146/i/450/depositphotos_51462919-stock-photo-happy-young-man-traveling-with.jpg"
+    ),
+    User(
+        "Dewi Rahayu",
+        "DewiRahayu@gmail.com",
+        "Hukum",
+        6,
+        "https://st2.depositphotos.com/3889193/6374/i/450/depositphotos_63745245-stock-photo-cute-student-girl-taking-notes.jpg"
+    ),
+    User(
+        "Hendra Wijaya",
+        "HendraWijaya@gmail.com",
+        "Ekonomi",
+        7,
+        "https://st4.depositphotos.com/1158045/19931/i/450/depositphotos_199311608-stock-photo-handsome-young-man-reading-book.jpg"
+    ),
+    User(
+        "Linda Sari",
+        "LindaSari@gmail.com",
+        "Kedokteran",
+        8,
+        "https://st2.depositphotos.com/4431055/11856/i/450/depositphotos_118562150-stock-photo-young-student-girl.jpg"
+    ),
+    User(
+        "Eko Nugroho",
+        "EkoNugroho@gmail.com",
+        "Arsitektur",
+        9,
+        "https://st4.depositphotos.com/12982378/25187/i/450/depositphotos_251879625-stock-photo-smiling-student-red-shirt-sitting.jpg"
+    ),
+    User(
+        "Dian Purnama",
+        "DianPurnama@gmail.com",
+        "Desain Grafis",
+        10,
+        "https://st.depositphotos.com/1594308/2419/i/450/depositphotos_24196475-stock-photo-student-with-copybook.jpg"
+    )
+)
+
+val userListB = listOf(
+    User(
+        "John Doe",
+        "john.doe@gmail.com",
+        "Teknik Sipil",
+        1,
+        "https://st4.depositphotos.com/12982378/25187/i/450/depositphotos_251879625-stock-photo-smiling-student-red-shirt-sitting.jpg"
+    ),
+    User(
+        "Jane Smith",
+        "jane.smith@gmail.com",
+        "Fisika",
+        2,
+        "https://st.depositphotos.com/1594308/2419/i/450/depositphotos_24196475-stock-photo-student-with-copybook.jpg"
+    ),
+    User(
+        "Michael Johnson",
+        "michael.johnson@gmail.com",
+        "Biologi",
+        3,
+        "https://st4.depositphotos.com/1158045/19931/i/450/depositphotos_199311608-stock-photo-handsome-young-man-reading-book.jpg"
+    ),
+    User(
+        "Emily Brown",
+        "emily.brown@gmail.com",
+        "Seni Rupa",
+        4,
+        "https://st2.depositphotos.com/4431055/11856/i/450/depositphotos_118562150-stock-photo-young-student-girl.jpg"
+    ),
+    User(
+        "David Davis",
+        "david.davis@gmail.com",
+        "Ilmu Politik",
+        5,
+        "https://st.depositphotos.com/1715570/5146/i/450/depositphotos_51462919-stock-photo-happy-young-man-traveling-with.jpg"
+    ),
+    User(
+        "Sarah Wilson",
+        "sarah.wilson@gmail.com",
+        "Teknik Elektro",
+        6,
+        "https://st2.depositphotos.com/3889193/6374/i/450/depositphotos_63745245-stock-photo-cute-student-girl-taking-notes.jpg"
+    ),
+    User(
+        "Robert Anderson",
+        "robert.anderson@gmail.com",
+        "Sastra Inggris",
+        7,
+        "https://st2.depositphotos.com/4431055/11853/i/450/depositphotos_118536368-stock-photo-student-man-with-laptop.jpg"
+    ),
+    User(
+        "Jessica Thomas",
+        "jessica.thomas@gmail.com",
+        "Psikologi",
+        8,
+        "https://st2.depositphotos.com/1594308/12210/i/450/depositphotos_122104332-stock-photo-female-college-student.jpg"
+    ),
+    User(
+        "Daniel White",
+        "daniel.white@gmail.com",
+        "Ekonomi Islam",
+        9,
+        "https://st.depositphotos.com/2931363/5142/i/450/depositphotos_51425493-stock-photo-student-holding-textbooks.jpg"
+    ),
+    User(
+        "Jennifer Lee",
+        "jennifer.lee@gmail.com",
+        "Desain Interior",
+        10,
+        "https://st.depositphotos.com/1594308/2419/i/450/depositphotos_24196475-stock-photo-student-with-copybook.jpg"
+    )
+)
