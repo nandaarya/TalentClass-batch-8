2a564e029cd1f38dc626fb1dd8b443fae9c45925
Nanda Arya Putra <nandaarya404@gmail.com>
2023-07-11 00:15:20 +0700

    hide token

--- Day 5/Tugas-Day-5-App/app/src/main/java/com/example/tugas_day_5_app/util/Constant.kt
+++ Day 5/Tugas-Day-5-App/app/src/main/java/com/example/tugas_day_5_app/util/Constant.kt
@@ -2,6 +2,6 @@ package com.example.tugas_day_5_app.util
 
 class Constant {
     companion object{
-        const val token = "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"
+        const val token = "masukkan token github disini"
     }
 }
