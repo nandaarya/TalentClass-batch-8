c3e95c3a416209e364a446a01abcb90fc59aa8d1
Nanda Arya Putra <nandaarya404@gmail.com>
2023-07-11 22:48:49 +0700

    initial commit

---
+++ Day 6/Tugas-Day-6-App/app/src/main/java/com/example/tugas_day_6_app/AddBookActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tugas_day_6_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tugas_day_6_app.R
+
+class AddBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_book)
+    }
+}
---
+++ Day 6/Tugas-Day-6-App/app/src/main/java/com/example/tugas_day_6_app/EditDetailBookActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tugas_day_6_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditDetailBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_detail_book)
+    }
+}
